f4024661bc5c28fa6be1f503bc27dc86a7fad1fd
kartondev <d.cherkasov@haulmont.com>
2024-11-07 13:11:34 +0300

    init commit

---
+++ pet-service/src/main/java/io/jmix/petservice/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package io.jmix.petservice.view.user;
+
+import io.jmix.petservice.entity.User;
+import io.jmix.petservice.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
